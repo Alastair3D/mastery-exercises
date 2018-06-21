f2b01774f22a8be0645821170af162da9954d8db
Alastair Edmonds <Alastair.Edmonds@gmail.com>
2018-06-21 14:40:21 +0100

    Adventure inx

--- adventure_game.rb
+++ adventure_game.rb
@@ -1,3 +1,19 @@
+# Chapter 6, Exercise 2: AdventureGame.rb
+#
+# Specification:  The user starts by facing forward. They can move right, left, or forward. If they move right, they die (there's a goblin). If they move left, they die (there's a werewolf). If they move forward, they live. They have to do this twice to win.
+# Breaking this down into requirements, where each requirement details one feature of the game:
+
+# 1.    The user is told they're facing forward and can type 'forward', 'left', or 'right'.
+# 2.    If the user enters 'right', they die (goblin).
+# 3.    If the user enters 'left', they die (werewolf).
+# 4.    If the user enters 'forward', they're told they live, and can move again.
+# 5.    If the user enters anything other than 'right', 'left', or 'forward', they just get the message from 1 again.
+# 6.    If the user enters 'right', they die (goblin).
+# 7.    If the user enters 'left', they die (werewolf).
+# 8.    If the user enters 'forward', they're told they won and the game exits.
+# 9.    If the user enters anything other than 'right', 'left', or 'forward', they just get the message from 4 again.
+
+
 move_number = 0
 
 while true do
