e73f7d3b1fad6dc9cedc7a36742d2aa9dd97510b
Alastair Edmonds <Alastair.Edmonds@gmail.com>
2018-06-21 14:36:50 +0100

    Add greetings.rb

---
+++ greetings.rb
@@ -0,0 +1,11 @@
+# Specification: a vague answer to the question, “What do you want this programme to do?”
+#
+# “I hate my mate Steve: so much so that my hatred spills over to anyone with a first name beginning with 'S'. I want a program that, when anyone types their name in, shouts at them if their name begins with an 'S'. Anyone else should just get a friendly greeting.”
+#
+# The process of breaking a specification down into requirements is known as algorithmic thinking.
+#
+# Here's the list of requirements for the specification above:
+# •    The user sees a greeting, which asks them to enter their name.
+# •    The user enters their name.
+# •    If the user's name begins with 'S', the program shouts the user's name back at them.
+# •    If the user's name begins with any other letter, the program just says 'Hi, ' plus their name.
