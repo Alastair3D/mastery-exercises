2a9f8a20de0809c49e40b5837565d3e9b6c37161
Alastair Edmonds <Alastair.Edmonds@gmail.com>
2018-06-21 14:27:19 +0100

    Add DiceGame inx

--- dice_game_1.rb
+++ dice_game_1.rb
@@ -1,3 +1,9 @@
+# Chapter 4, Exercise 1: DiceGame
+#
+# Use ‘rand’ to build a dice game.  
+# Rules are: if I roll higher than my opponent, I win.
+
+
  player_1 = rand(1..6)
   player_2 = rand(1..6)
   if player_1 > player_2
@@ -6,4 +12,4 @@
     puts "player 2 wins"
   else
     puts "draw"
-  end		
+  end
