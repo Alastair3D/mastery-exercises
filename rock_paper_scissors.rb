4a7768c077d86b60b5acc46d9036c8c3f52cb7b2
Alastair Edmonds <Alastair.Edmonds@gmail.com>
2018-06-21 14:44:40 +0100

    Add RPS

--- rock_paper_scissors.rb
+++ rock_paper_scissors.rb
@@ -6,3 +6,24 @@
 # * Calculates who has won.  `rock` beats `scissors`, `paper` beats `rock`, `scissors` beat `paper`.
 # * If player 1 has won, `puts`es `Player 1 wins`.  * If player 2 has won, `puts`es `Player 2 wins`. * If the game is a draw, `puts`es `It's a draw`.
 # * Note: When you run the automated tests, the tests will simulate the user input.
+
+while true do
+
+puts "Player 1, your move please"
+p1 = gets.chomp
+
+puts "Player 2, your move please"
+p2 = gets.chomp
+
+wins = {'scissors' => 'paper', 'paper' => 'rock', 'rock' => 'scissors'}
+
+if p1 == p2
+ puts "It's a draw"
+elsif wins[p1] == p2
+ puts "Player 1 wins"
+ break
+elsif wins[p2] == p1
+ puts "Player 2 wins"
+ break
+end
+end
