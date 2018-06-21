09cec4aa299b04c2537c7fdde6b042b9c0a01780
Alastair Edmonds <Alastair.Edmonds@gmail.com>
2018-06-21 14:44:14 +0100

    Add RPS inx

---
+++ rock_paper_scissors.rb
@@ -0,0 +1,8 @@
+# Chapter 6, Quiz 2
+
+# Q2. Write a program that lets two players play Rock, Paper, Scissors. The program should:
+# * Ask player 1 for their move.  They can input `rock`, `paper` or `scissors`.
+# * Ask player 2 for their move.  They can input `rock`, `paper` or  `scissors`.
+# * Calculates who has won.  `rock` beats `scissors`, `paper` beats `rock`, `scissors` beat `paper`.
+# * If player 1 has won, `puts`es `Player 1 wins`.  * If player 2 has won, `puts`es `Player 2 wins`. * If the game is a draw, `puts`es `It's a draw`.
+# * Note: When you run the automated tests, the tests will simulate the user input.
